700616ffaab5205cfcb0dea9c8924b73fb9e9bc2
asyaasha <asya.asha@gmail.com>
2017-08-18 17:41:25 -0400

    create earth object

--- js/game.js
+++ js/game.js
@@ -8,14 +8,15 @@ var Colors = {
     blue:0x68c3c0,
 };
 
-var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane,  HEIGHT, WIDTH, renderer, container;
+var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane,  
+  HEIGHT, WIDTH, renderer, container, hemisphereLight, shadowLight;
 
 function createScene(){
+
   HEIGHT = window.innerHeight;
   WIDTH = window.innerWidth;
 
   scene = new THREE.Scene();
-
   scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
 
   //create camera
@@ -40,15 +41,66 @@ renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(WIDTH, HEIGHT);
 renderer.shadowMap.enabled = true;
-
-//add the DOM element of the renderer to the container in html
-container = document.getElementById('world');
+container = document.getElementById('world');//add the DOM element of the renderer to the container in html
 container.appendChild(renderer.domElement);
-//listen to the screen, update it on resize
-window.addEventListener('resize', handleWindowResize, false);
 
+window.addEventListener('resize', handleWindowResize, false);
 }
 
+function handleWindowResize() {   //update on resize
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+}
+
+function createLights(){
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9);
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+  //the direction of the light  
+  shadowLight.position.set(150, 350, 350);
+  //shadows casting 
+  shadowLight.castShadow = true;
+  // define the visible area of the projected shadow
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+
+  // define the resolution of the shadow; the higher the better, 
+  // but also the more expensive and less performant
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+  
+  // add lights to the scene to activate
+  scene.add(hemisphereLight);  
+  scene.add(shadowLight);
+}
+
+Earth = function(){
+  var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+  geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2)); //rotate on x axis
+
+  var mat = new THREE.MeshPhongMaterial({
+    color:Colors.brown,
+    transparent:true,
+    opacity:.6,
+    shading:THREE.FlatShading,
+  });
+
+  this.mesh = new THREE.Mesh(geom,mat);  //mesh combination of geometry and material
+  this.mesh.recieveShadow = true;
+}
+var earth;
+function createEarth(){
+earth = new Earth();
+earth.mesh.position.y = -600;
+scene.add(earth.mesh);
+}
+renderer.render(scene, camera);
 function init(){
   //set up the scene
   createScene();
