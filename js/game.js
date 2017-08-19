f2c8f28a3770c9424eebc88987027fa331d3e563
asyaasha <asya.asha@gmail.com>
2017-08-18 21:06:25 -0400

    create sky

--- js/game.js
+++ js/game.js
@@ -1,11 +1,12 @@
 //COLORS
 var Colors = {
     red:0xf25346,
-    white:0xd8d0d1,
+    white:0xffffff,
     brown:0x59332e,
     pink:0xF5986E,
     brownDark:0x23190f,
     blue:0x68c3c0,
+    green:0xadff2f,
 };
 
 var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane,  
@@ -17,7 +18,7 @@ function createScene(){
   WIDTH = window.innerWidth;
 
   scene = new THREE.Scene();
-  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+  scene.fog = new THREE.Fog(0xf7cd68, 100, 1000);
 
   //create camera
   aspectRatio = WIDTH/HEIGHT;
@@ -70,15 +71,75 @@ function createLights(){
   scene.add(shadowLight);
 }
 
+//***** CREATE CLOUDS *******
+
+Cloud = function(){
+  // Create an empty container that will hold the different parts of the cloud
+  this.mesh = new THREE.Object3D();
+  var geom = new THREE.SphereGeometry( 20, 20, 20 );
+  
+  var mat = new THREE.MeshPhongMaterial({
+    color:Colors.white,  
+  });
+  
+  // duplicate the geometry a random number of times
+  var nBlocs = 3+Math.floor(Math.random()*3);
+  for (var i=0; i<nBlocs; i++ ){
+    
+    var m = new THREE.Mesh(geom, mat);     // create the mesh by cloning the geometry
+    m.position.x = i*15;                // set the position and the rotation randomly
+    m.position.y = Math.random()*10;
+    m.position.z = Math.random()*10;
+    m.rotation.z = Math.random()*Math.PI*2;
+    m.rotation.y = Math.random()*Math.PI*2;
+    var s = .1 + Math.random()*.9;      // set the size randomly
+    m.scale.set(s,s,s);
+    m.castShadow = true;
+    m.receiveShadow = true;
+    
+    // add to the container 
+    this.mesh.add(m);
+  } 
+}
+
+// ***** CREATE SKY ********
+
+Sky = function(){
+
+  this.mesh = new THREE.Object3D();  //empty container
+  this.numberClouds = 30;    
+  var stepAngle = Math.PI*2 / this.numberClouds;    // To distribute the clouds consistently they need to be placed accordingly to a uniform angle
+  
+  for(var i=0; i<this.numberClouds; i++){    // create the clouds
+    var c = new Cloud();
+    var a = stepAngle*i; // this is the final angle of the cloud
+    var h = 750 + Math.random()*200; // this is the distance between the center of the axis and the cloud itselF
+    c.mesh.position.y = Math.sin(a)*h;
+    c.mesh.position.x = Math.cos(a)*h;
+    c.mesh.rotation.z = a + Math.PI/2;              // rotate the cloud according to its position
+    c.mesh.position.z = -400-Math.random()*400;      // random depths inside of the scene
+    var s = 1+Math.random()*2;    // random scale for each cloud
+    c.mesh.scale.set(s,s,s);
+    this.mesh.add(c.mesh);  //add the mesh of each cloud in the scene
+  }  
+}
+var sky;
+function createSky(){
+  sky = new Sky();
+  sky.mesh.position.y = -550;
+  scene.add(sky.mesh);
+}
+
+//***** CREATE EARTH *******
+
 Earth = function(){
   var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
   geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2)); //rotate on x axis
 
-  var mat = new THREE.MeshPhongMaterial({
-    color:Colors.brown,
+  var mat = new THREE.MeshBasicMaterial({
+    color:Colors.green,
     transparent:true,
     opacity:.6,
-    shading:THREE.FlatShading,
   });
 
   this.mesh = new THREE.Mesh(geom,mat);  //mesh combination of geometry and material
@@ -90,7 +151,9 @@ earth = new Earth();
 earth.mesh.position.y = -600;
 scene.add(earth.mesh);
 }
+
 function loop(){
+  sky.mesh.rotation.z += .007;
   earth.mesh.rotation.z += .005;
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
@@ -100,7 +163,7 @@ function init(){
   createLights();
   // createPlane();//add objects
   createEarth();
-  // createSky();
+  createSky();
   loop();
 }
 window.addEventListener('load', init, false);
